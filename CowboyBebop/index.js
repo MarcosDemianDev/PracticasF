53fb12f72728558ffdca3b68fe894350b9f935bf
MarcosDemianDev <mdemian@hotmail.com>
2025-03-19 15:58:29 -0300

    add proyect

---
+++ CowboyBebop/index.js
@@ -0,0 +1,61 @@
+const container = document.querySelector('.container');
+let isDragging = false;
+let rotationX = 0;
+let rotationY = 0;
+const sensitivity = 0.2; 
+const maxRotation = 25; 
+container.addEventListener('mousedown', () => {
+    isDragging = true;
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        rotationX = (e.clientY - window.innerHeight / 2) / (window.innerHeight / 2) * maxRotation;
+        rotationY = (e.clientX - window.innerWidth / 2) / (window.innerWidth / 2) * maxRotation;
+
+        rotationX = Math.max(Math.min(rotationX, maxRotation), -maxRotation);
+        rotationY = Math.max(Math.min(rotationY, maxRotation), -maxRotation);
+
+        container.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    }
+});
+
+if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', (e) => {
+        const beta = e.beta;
+        const gamma = e.gamma;
+        const maxTilt = 15;
+
+        rotationX = Math.max(Math.min(beta, maxTilt), -maxTilt);
+        rotationY = Math.max(Math.min(gamma, maxTilt), -maxTilt);
+
+        container.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    });
+} else {
+    console.log("Tu dispositivo no soporta el evento de orientación.");
+}
+
+const starsContainer = document.querySelector('.stars');
+
+function createStars(numStars) {
+    for (let i = 0; i < numStars; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+
+        const x = Math.random() * 100;
+        const y = Math.random() * 30; 
+        star.style.left = `${x}%`;
+        star.style.top = `${y}%`;
+
+        const duration = Math.random() * 2 + 1;
+        star.style.animationDuration = `${duration}s`;
+
+        starsContainer.appendChild(star);
+    }
+}
+
+createStars(30);
---
+++ README.md
@@ -0,0 +1,16 @@
+**Repositorio de Práctica Front-End**  
+Este repositorio contiene pequeños proyectos y ejercicios de práctica en desarrollo front-end. Cada proyecto está diseñado para explorar y mejorar habilidades en HTML, CSS, JavaScript y frameworks modernos. ¡Perfecto para aprender, experimentar y construir un portafolio!
+
+🌟  **Proyectos incluidos**:
+
+-   Efectos visuales con CSS.
+    
+-   Interacciones dinámicas con JavaScript.
+    
+-   Diseños responsivos y modernos.
+    
+
+🚀  **Objetivo**:  
+Reforzar conceptos, probar nuevas tecnologías y compartir soluciones creativas.
+
+¡Contribuciones, sugerencias y feedback son bienvenidos!
